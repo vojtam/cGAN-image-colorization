ff487a5f16376dd271e51c57cdab6f32f8068a4f
vojtam <macala.vojtech@gmail.com>
2025-03-21 10:58:24 +0100

    feat: implemented torch dataset

--- dataset.py
+++ dataset.py
@@ -1,16 +1,40 @@
-# STUDENT's UCO: 000000
+# STUDENT's UCO: 505941
 
 # Description:
 # This file should contain custom dataset class. The class should subclass the torch.utils.data.Dataset.
 
-import torch
 from torch import Tensor
 from torch.utils.data import Dataset
+from pathlib import Path
+from PIL import Image
+from torchvision.transforms.functional import pil_to_tensor
+
+class ImageDataset(Dataset[Tensor]):
+
+    def __init__(self, dataset_path: Path, transform=None):
+        self.img_paths = self._get_image_paths(dataset_path)
+        self.dataset_path = dataset_path
+        self.transform = transform
+
+    def _get_image_paths(self, dataset_path: Path) -> list[str]:
+        
+        img_paths = []
+        for dir in dataset_path.iterdir():
+            if dir.is_dir():
+                for image in dir.iterdir():
+                    if image.suffix == ".png":
+                        img_paths.append(image.as_posix()) 
+        return img_paths
 
 
-class SampleDataset(Dataset[Tensor]):
     def __len__(self) -> int:
-        return 1
+        return len(self.img_paths)
 
     def __getitem__(self, idx: int) -> Tensor:
-        return torch.zeros((256, 256))
+        img_path = self.img_paths[idx]
+        img = pil_to_tensor(Image.open(img_path)) / 255
+
+        if self.transform is not None:
+            img = img.permute(1, 2, 0).numpy()
+            img = self.transform(image=img)['image']
+        return img
