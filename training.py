ef106465a7218a3ed90c354969e253c0c32bec82
vojtam <macala.vojtech@gmail.com>
2025-03-27 14:18:54 +0000

    feat: torch compile + other optims

--- training.py
+++ training.py
@@ -4,21 +4,20 @@
 # This file should be used for performing training of a network
 # Usage: python training.py <dataset_path>
 
+import time
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from pathlib import Path
 
-import albumentations as A
 import matplotlib.pyplot as plt
 import torch
-from albumentations.pytorch import ToTensorV2
 from torch import Tensor, nn
 from torch.optim import Adam, Optimizer
-from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import DataLoader
 from torchview import draw_graph
 from tqdm import tqdm
 
-from dataset import ImageDataset, WrappedDataLoader
+from dataset import WrappedDataLoader, get_image_paths_df, split_dataset
 from network import Discriminator, Generator, discriminator_loss, generator_loss
 
 
@@ -56,8 +55,16 @@ def plot_learning_curves(
     plt.savefig("learning_curves.png")
 
 
+@dataclass
+class FitStepResult:
+    total_G_loss: float
+    G_loss: float
+    G_l1_loss: float
+    D_loss: float
+
+
 def generate_image(
-    model: Generator, input: Tensor, target: Tensor, plot_subtitle: str = ""
+    model: Generator, input: Tensor, target: Tensor, epoch: int, plot_subtitle: str = ""
 ):
     # input has a shape C x H x W
     # target has a shape C x H x W
@@ -81,11 +88,12 @@ def generate_image(
             plt.imshow(display_list[i] * 0.5 + 0.5)
         plt.axis("off")
     plt.suptitle(plot_subtitle)
+    path = f"gen_images/generated_image_{epoch}.png"
+    print(f"Sample colorized image saved to {path}")
+    plt.savefig(path)
 
 
 # sample function for training
-
-
 def fit(
     G: Generator,
     D: Discriminator,
@@ -94,6 +102,8 @@ def fit(
     val_dataloader: DataLoader,
     optimizer_G: Optimizer,
     optimizer_D: Optimizer,
+    input_to_vizu: Tensor,
+    target_to_vizu: Tensor,
     device: torch.device,
 ) -> tuple[list[float], list[float]]:
     G_train_losses: list[float] = []
@@ -107,71 +117,60 @@ def fit(
     for epoch in range(epochs):
         G.train()
         D.train()
+        torch.cuda.synchronize()
+        epoch_start_time = time.time()
+
+        train_G_loss = 0.0
+        train_D_loss = 0.0
+        train_L1_loss = 0.0
 
         for inputs, targets in tqdm(train_dataloader):
             step_result = fit_step(G, D, optimizer_G, optimizer_D, inputs, targets)
 
-            # graph variables
-            G_train_losses.append(step_result.total_G_loss.cpu())
-            D_train_losses.append(step_result.D_loss.cpu())
-            L1_train_losses.append(step_result.G_l1_loss.cpu())
+            train_G_loss += step_result.total_G_loss.item()
+            train_D_loss += step_result.D_loss.item()
+            train_L1_loss += step_result.G_l1_loss.item()
+
+        n_train_batches = len(train_dataloader)
+        G_train_losses.append(train_G_loss / n_train_batches)
+        D_train_losses.append(train_D_loss / n_train_batches)
+        L1_train_losses.append(train_L1_loss / n_train_batches)
 
         G.eval()
         D.eval()
+
+        val_G_loss = 0.0
+        val_D_loss = 0.0
+        val_L1_loss = 0.0
+        n_val_batches = len(val_dataloader)
+
         with torch.no_grad():
             for inputs, targets in tqdm(val_dataloader):
                 step_result = fit_step(
                     G, D, None, None, inputs, targets
                 )  # optimizers are none -> don't update
 
-                # graph variables
-                G_val_losses.append(step_result.total_G_loss)
-                D_val_losses.append(step_result.D_loss)
-                L1_val_losses.append(step_result.G_l1_loss)
+                val_G_loss += step_result.total_G_loss.item()
+                val_D_loss += step_result.D_loss.item()
+                val_L1_loss += step_result.G_l1_loss.item()
 
-        # print training
+        G_val_losses.append(val_G_loss / n_val_batches)
+        D_val_losses.append(val_D_loss / n_val_batches)
+        L1_val_losses.append(val_L1_loss / n_val_batches)
+
+        torch.cuda.synchronize()
+        epoch_end_time = time.time()
 
         print(
-            f"Epoch: {epoch}, train_G_loss: {G_train_losses[-1]:.3f} | train_L1_loss: {L1_train_losses[-1]:.3f} | train_D_loss: {D_train_losses[-1]:.3f}"
+            f"Epoch: {epoch}, time_elapsed: {epoch_end_time - epoch_start_time:.2f} seconds | ",
+            f"train_G_loss: {train_G_loss:.3f} | train_L1_loss: {train_L1_loss:.3f} | train_D_loss: {train_D_loss:.3f} | ",
+            f"val_G_loss: {val_G_loss:.3f} | val_L1_loss: {val_L1_loss:.3f} | val_D_loss: {val_D_loss:.3f}",
+            sep="\n",
         )
 
-        print(
-            f"Epoch: {epoch}, val_G_loss: {G_val_losses[-1]:.3f} | val_L1_loss: {L1_val_losses[-1]:.3f} | val_D_loss: {D_val_losses[-1]:.3f}"
-        )
-        print(inputs[-1].shape)
-        generate_image(G, inputs[-1], targets[-1], f"epoch: {epoch}")
+        generate_image(G, input_to_vizu, target_to_vizu, epoch, f"epoch: {epoch}")
     print("Training finished!")
     return G_train_losses, D_train_losses
-
-
-@dataclass
-class FitStepResult:
-    total_G_loss: float
-    G_loss: float
-    G_l1_loss: float
-    D_loss: float
-
-
-def split_dataset(
-    dataset: Dataset, test_size: float = 0.15, val_size: float = 0.10
-) -> tuple[Dataset, Dataset, Dataset]:
-    train_ratio = 1.0 - test_size
-
-    train_size, test_size = (
-        int(len(dataset) * train_ratio),
-        len(dataset) - (int(len(dataset) * train_ratio)),
-    )
-
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-    val_ratio = 1.0 - val_size
-    train_size, val_size = (
-        int(len(train_dataset) * val_ratio),
-        len(train_dataset) - (int(len(train_dataset) * val_ratio)),
-    )
-
-    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
-
-    return (train_dataset, val_dataset, test_dataset)
 
 
 def fit_step(
@@ -196,11 +195,10 @@ def fit_step(
         discriminator_real_output, discriminator_generated_output
     )
 
-    with torch.autograd.detect_anomaly():
-        if optimizer_G is not None:
-            optimizer_G.zero_grad()
-            total_G_loss.backward()
-            optimizer_G.step()
+    if optimizer_G is not None:
+        optimizer_G.zero_grad()
+        total_G_loss.backward()
+        optimizer_G.step()
 
     if optimizer_D is not None:
         optimizer_D.zero_grad()
@@ -208,22 +206,6 @@ def fit_step(
         optimizer_D.step()
 
     return FitStepResult(total_G_loss, G_loss, G_l1_loss, D_loss)
-
-
-def get_train_transforms():
-    train_transform = A.Compose(
-        [
-            A.Resize(600, 420),
-            A.RandomCrop(512, 384),  # 4x3
-            A.HorizontalFlip(p=0.5),
-            A.Rotate(limit=30, p=0.5),
-            A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.2),
-            A.Normalize(normalization="min_max"),
-            ToTensorV2(),
-        ],
-        additional_targets={"rgb_image": "image"},
-    )
-    return train_transform
 
 
 # declaration for this function should not be changed
@@ -242,11 +224,11 @@ def training(dataset_path: Path) -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Computing with {}!".format(device))
 
-    batch_size = 4
+    batch_size = 128
 
-    dataset = ImageDataset(dataset_path, get_train_transforms(), n=40)
+    img_paths_df = get_image_paths_df(dataset_path)
 
-    train_dataset, val_dataset, _ = split_dataset(dataset, 0.15, 0.1)
+    train_dataset, val_dataset, _ = split_dataset(img_paths_df, 0.15, 0.1)
 
     train_dataloader = WrappedDataLoader(
         DataLoader(train_dataset, batch_size=batch_size), device
@@ -255,8 +237,15 @@ def training(dataset_path: Path) -> None:
         DataLoader(val_dataset, batch_size=batch_size, shuffle=True), device
     )
 
-    D = Discriminator()
-    G = Generator()
+    torch.set_float32_matmul_precision("high")
+
+    D = Discriminator().to(device)
+    G = Generator().to(device)
+
+    D, G = (
+        torch.compile(D, mode="reduce-overhead"),
+        torch.compile(G, mode="reduce-overhead"),
+    )
 
     # define optimizer and learning rate
     optimizer_G = Adam(G.parameters(), lr=config.lr, betas=config.momentum_betas)
@@ -265,7 +254,16 @@ def training(dataset_path: Path) -> None:
 
     # train the network
     train_losses, val_losses = fit(
-        G, D, 3, train_dataloader, val_dataloader, optimizer_G, optimizer_D, device
+        G,
+        D,
+        3,
+        train_dataloader,
+        val_dataloader,
+        optimizer_G,
+        optimizer_D,
+        val_dataset[0][0].to(device),
+        val_dataset[0][1].to(device),
+        device,
     )
 
 
